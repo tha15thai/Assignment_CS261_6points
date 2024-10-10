5e6e2c056246d20a4434638ed937fc0a32a9c25b
tha15thai <thawanhathai.thi@dome.tu.ac.th>
2024-10-11 02:31:55 +0700

    update directory Frontend

--- Frontend/server.js
+++ Frontend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 const PORT = process.env.PORT || 3000;
